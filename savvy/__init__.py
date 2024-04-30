27331eca97ff803464e07eb8682892cb0f02103b
idm <idm@savvydefi.io>
2024-04-30 16:03:37 -0400

    tests passing

--- savvy/__init__.py
+++ savvy/__init__.py
@@ -31,8 +31,11 @@ class BusinessModel:
         """
         month is the month number
         """
+        if month == 1:
+            return self.starting_deposits
+
         total_deposits = self.starting_deposits
-        for i in range(month):
+        for i in range(month-1):
             net_deposits = total_deposits * self.growth_pct
             total_deposits += net_deposits
         return total_deposits
@@ -41,11 +44,11 @@ class BusinessModel:
         """
         month is the month number
         """
-        if month <= 0:
+        if month <= 1:
             return 0
 
         total_deposits = self.starting_deposits
-        for i in range(month):
+        for i in range(month-1):
             net_deposits = total_deposits * self.growth_pct
             total_deposits += net_deposits
         return net_deposits
@@ -54,14 +57,21 @@ class BusinessModel:
         """
         month is the month number
         """
+        if month <= 0:
+            return 0
+        
         total_sages = 0
-        for i in range(month):
-            deposits_last_month = self.calc_total_deposits(month-1)
-            customer_yield = deposits_last_month * self.average_user_yield * (1-self.protocol_fee_pct) / self.periods_in_year
+        for i in range(1, month+1):
+            if i == 1:
+                deposits = self.calc_total_deposits(1)
+            else:
+                deposits = self.calc_total_deposits(i-1)
+
+            customer_yield = deposits * self.average_user_yield * (1-self.protocol_fee_pct) / self.periods_in_year
             total_sages += customer_yield
         return total_sages
 
-    def calc_net_new_pol(self, month):
+    def calc_net_new_pol(self, month, cumulative=False):
         """
         month is the month number
         """
@@ -71,59 +81,93 @@ class BusinessModel:
         total_treasury_pol = self.starting_pol
         total_treasury_previous_month = total_treasury_pol
 
-        for i in range(month):
-            protocol_fee_from_deposits = self.calc_total_deposits(month-1) * self.average_user_yield * self.protocol_fee_pct / self.periods_in_year
+        if month == 1:
+            return total_treasury_pol
+
+        for i in range(1, month+1):
+            protocol_fee_from_deposits = self.calc_total_deposits(i-1) * self.average_user_yield * self.protocol_fee_pct / self.periods_in_year
+
             treasury_yield_on_yield = total_treasury_previous_month * self.average_protocol_yield / self.periods_in_year
-            sage_yield_on_yield = self.calc_total_sages(month-1) * self.average_protocol_yield / self.periods_in_year
+
+            sage_yield_on_yield = self.calc_total_sages(i-1) * self.average_protocol_yield / self.periods_in_year
+
             net_new = protocol_fee_from_deposits + treasury_yield_on_yield + sage_yield_on_yield
+
             total_treasury_pol += net_new
             total_treasury_previous_month = total_treasury_pol
 
-        return net_new
+        if cumulative:
+            return total_treasury_pol
+        else:
+            return net_new
 
     def calc_total_treasury(self, month):
         """
         month is the month number
         """
-        if month <= 0:
-            return 0
+        if month == 1:
+            return self.starting_pol
 
-        total_treasury_pol = self.starting_pol
-        for i in range(month):
-            total_treasury_pol += self.calc_net_new_pol(month)
-
-        return total_treasury_pol
+        buybacks = self.calc_buybacks(month-1)
+        last_month = self.calc_total_treasury(month-1)
+        net_new_pol = self.calc_net_new_pol(month)
+        return last_month + net_new_pol - buybacks
 
     def calc_buybacks(self, month):
         """
         month is the month number
         """
-        buybacks = 0
-        for i in range(month):
-            buybacks_this_month = self.calc_net_new_pol(month) * self.buyback_rate_pct
-            buybacks += buybacks_this_month
-        return buybacks
+        net_new_pol = self.calc_net_new_pol(month)
+        return net_new_pol * self.buyback_rate_pct
 
-    def calc_lp_rewards(self, month):
+    def calc_cost_of_lp(self, month):
         """
         month is the month number
         """
-        total_credit_lines = self.starting_credit_lines
-        cumulative_credit_lines = 0
-
-        for i in range(month):
-            net_new_deposits = self.calc_net_new_deposits(month)
+        cumulative_base_asset_required = self.starting_credit_lines
+        for i in range(1, month+1):
+            net_new_deposits = self.calc_net_new_deposits(i)
             new_credit_lines = net_new_deposits * self.credit_utilization
-            monthly_swap_pressure = net_new_deposits * self.monthly_swap_pressure
-            cumulative_credit_lines += new_credit_lines
+            cumulative_base_asset_required += new_credit_lines
 
-        return new_credit_lines * self.lp_expected_apr / self.periods_in_year
+
+        return total_treasury * self.lp_expected_apr / self.periods_in_year
+
+    def calc_monthly_swap_pressure(self, month):
+        """
+        month is the month number
+        """
+        if month == 1:
+            return self.starting_credit_lines
+        return self.calc_new_credit_lines(month) * self.monthly_swap_pressure
+
+    def calc_new_credit_lines(self, month):
+        if month == 1:
+            return self.starting_credit_lines
+        return self.calc_net_new_deposits(month) * self.credit_utilization
+
+    def calc_cumulative_base_asset_required(self, month):
+        """
+        month is the month number
+        """
+        if month == 1:
+            return self.starting_credit_lines
+
+        cumulative_previous = self.calc_cumulative_base_asset_required(month-1)
+        new_base_asset_required = self.calc_monthly_swap_pressure(month)
+        return cumulative_previous + new_base_asset_required
+
+    def calc_cost_of_lp(self, month):
+        """
+        month is the month number
+        """
+        return self.calc_cumulative_base_asset_required(month) * self.lp_expected_apr / self.periods_in_year
 
     def calc_net_zero(self, month):
         """
         month is the month number
         """
-        return self.calc_buybacks(month) - self.calc_lp_rewards(month)
+        return self.calc_buybacks(month) - self.calc_cost_of_lp(month)
 
     def run(self):
         self._net_zero = self.calc_net_zero(12)
--- tests/test_model.py
+++ tests/test_model.py
@@ -3,49 +3,63 @@ import pytest
 from savvy import BusinessModel
 
 def test_create(business_model):
-    assert business_model.deposits_total == 1_000_000
+    assert business_model.starting_deposits == 1_000_000
 
 def test_calc_total_deposits(business_model):
-    assert business_model.calc_total_deposits(month=0) == 1_000_000
-    assert business_model.calc_total_deposits(month=1) == 1_100_000
-    assert business_model.calc_total_deposits(month=2) == 1_210_000
-    assert business_model.calc_total_deposits(month=3) == 1_331_000
+    assert business_model.calc_total_deposits(month=1) == 1_000_000
+    assert business_model.calc_total_deposits(month=2) == 1_100_000
+    assert business_model.calc_total_deposits(month=3) == 1_210_000
 
 def test_calc_net_new_deposits(business_model):
-    assert business_model.calc_net_new_deposits(month=0) == 0
-    assert business_model.calc_net_new_deposits(month=1) == 100_000
-    assert business_model.calc_net_new_deposits(month=2) == 110_000
-    assert business_model.calc_net_new_deposits(month=3) == 121_000
+    assert business_model.calc_net_new_deposits(month=1) == 0
+    assert business_model.calc_net_new_deposits(month=2) == 100_000
+    assert business_model.calc_net_new_deposits(month=3) == 110_000
 
 def test_calc_total_sages(business_model):
-    assert business_model.calc_total_sages(month=1) == 6_000
-    assert business_model.calc_total_sages(month=2) == 13_200
-    assert business_model.calc_total_sages(month=3) == 21_780
+    assert business_model.calc_total_sages(month=1) == 9_000
+    assert business_model.calc_total_sages(month=2) == 18_000
+    assert business_model.calc_total_sages(month=3) == 27_900
 
 def test_calc_total_treasury(business_model):
-    assert business_model.calc_total_treasury(month=1) == 8_066.666666666666
-    assert business_model.calc_total_treasury(month=2) == 13_285.555555555555
-    assert business_model.calc_total_treasury(month=3) == 19_896.93888888889
+    assert business_model.calc_total_treasury(month=1) == 1_000
+    assert business_model.calc_total_treasury(month=2) == pytest.approx(1_867, 50)
+    assert business_model.calc_total_treasury(month=3) == pytest.approx(2_948, 50)
 
 def test_calc_net_new_pol(business_model):
-    assert business_model.calc_net_new_pol(month=1) == 4_066.6666666666665
-    assert business_model.calc_net_new_pol(month=2) == 4_642.777777777777
-    assert business_model.calc_net_new_pol(month=3) == 5_298.97962962963
+    assert business_model.calc_net_new_pol(month=1) == 1_000
+    assert business_model.calc_net_new_pol(month=2) == pytest.approx(1_167, 1)
+    assert business_model.calc_net_new_pol(month=3) == pytest.approx(1_431, 10)
+    assert business_model.calc_net_new_pol(month=4) == pytest.approx(1_724, 20)
 
 def test_calc_buybacks(business_model):
-    assert business_model.calc_buybacks(month=1) == 1220.0
-    assert business_model.calc_buybacks(month=2) == 2785.6666666666665
-    assert business_model.calc_buybacks(month=3) == 4769.081666666667
+    assert business_model.calc_buybacks(month=1) == 300
+    assert business_model.calc_buybacks(month=2) == pytest.approx(350, 6)
+    assert business_model.calc_buybacks(month=3) == pytest.approx(429, 10)
 
-def test_calc_lp_rewards(business_model):
-    assert business_model.calc_lp_rewards(month=1) == 500
-    assert business_model.calc_lp_rewards(month=2) == 1100.0
-    assert business_model.calc_lp_rewards(month=3) == 1815.0
+def test_calc_cost_of_lp(business_model):
+    assert business_model.calc_cost_of_lp(month=1) == 5000
+    assert business_model.calc_cost_of_lp(month=2) == 5500
+    assert business_model.calc_cost_of_lp(month=3) == 6050
 
 def test_calc_net_zero(business_model):
-    assert business_model.calc_net_zero(month=1) == 0
-    assert business_model.calc_net_zero(month=2) == 0
-    assert business_model.calc_net_zero(month=3) == 0
+    assert business_model.calc_net_zero(month=1) == -4_700
+    assert business_model.calc_net_zero(month=2) == pytest.approx(-5_150, 10)
+    assert business_model.calc_net_zero(month=3) == pytest.approx(-5_621, 10)
+
+def test_calc_cumulative_base_asset_required(business_model):
+    assert business_model.calc_cumulative_base_asset_required(month=1) == 500_000
+    assert business_model.calc_cumulative_base_asset_required(month=2) == 550_000
+    assert business_model.calc_cumulative_base_asset_required(month=3) == 605_000
+
+def test_calc_monthly_swap_pressure(business_model):
+    assert business_model.calc_monthly_swap_pressure(month=1) == 500_000
+    assert business_model.calc_monthly_swap_pressure(month=2) == 50_000
+    assert business_model.calc_monthly_swap_pressure(month=3) == 55_000
+
+def test_calc_new_credit_lines(business_model):
+    assert business_model.calc_new_credit_lines(month=1) == 500_000
+    assert business_model.calc_new_credit_lines(month=2) == 50_000
+    assert business_model.calc_new_credit_lines(month=3) == 55_000
 
 @pytest.fixture
 def business_model():
@@ -53,9 +67,9 @@ def business_model():
         starting_deposits=1_000_000,
         growth_pct=0.1,
         average_user_yield=0.12,
-        starting_pol=4_000,
+        starting_pol=1000,
         average_protocol_yield=0.2,
-        protocol_fee_pct=0.4,
+        protocol_fee_pct=0.1,
         buyback_rate_pct=0.3,
         expected_apr=0.12,
     )
